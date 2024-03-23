78a8f6d6fd40a57710e85774f912539f3c26d288
Joydeep <joydeepdebnath868@gmail.com>
2024-03-23 12:03:56 +0530

    First Commit

---
+++ Responses.js
@@ -0,0 +1,53 @@
+export class ApiResponse {
+   statusCode
+   message
+   data
+   success
+  constructor(statusCode , message , data){
+      this.statusCode= statusCode
+      this.message= message
+      this.data= data
+      this.success= true
+  }
+  response(res){
+    return res.status(200).json({statusCode: this.statusCode ,success: this.success,  message:this.message , data:this.data })
+  }
+}
+export class ApiErrorResponse  extends Error{
+  statusCode
+  message
+  stack
+  errors
+  success
+  constructor(statusCode , message , stack , errors=[]){
+      super(message)
+      this.statusCode = statusCode
+      this.message = message
+      this.errors = errors
+      this.success = false
+      if(stack){
+          this.stack = stack
+      }
+      else{
+          Error.captureStackTrace(this , this.constructor)
+      }
+  }
+}
+
+
+
+
+export  function globleErrorHandler(err, req, res, next) {
+  if(err instanceof ApiErrorResponse){
+      return res.status(err.statusCode).json({
+          statusCode:err.statusCode,
+        message: err.message,
+          success:err.success
+      });
+  }
+  return res.status(500).json({
+      statusCode:500,
+      message:"Something went wrong from our side ",
+      success:false
+  })
+}
---
+++ index.js
@@ -0,0 +1,115 @@
+import express from 'express'
+import mongoose, { Schema, model } from 'mongoose'
+import { ApiErrorResponse, ApiResponse, globleErrorHandler } from './Responses.js'
+import cors from 'cors'
+
+
+const App = express()
+
+App.use(express.json())
+App.use(express.static("Public"))
+App.use(express.urlencoded({extended:false}))
+App.listen(3000 , ()=>{
+  console.log("Connected")
+
+})
+App.use(cors({
+  origin:"*"
+}))
+
+try {
+  await mongoose.connect("mongodb+srv://joydeep:Joydeep122@cluster0.zy4wbxw.mongodb.net/")
+  console.log("Mongoose Connected")
+} catch (error) {
+  console.log("Failed To Connect Mongoose ")
+  
+}
+
+
+const FormUser = new Schema({
+  email:{
+    type:String,
+    index:true , 
+    unique:true    
+  },
+  phone:{
+    type:String,
+    index:true , 
+    unique:true    
+  },
+  name:{
+    type:String,
+  },
+  school:{
+    type:String,
+    required:true
+
+  },
+  address:{
+    type:String,
+    enum:["Agartala" , "Outside Agartala"],
+    default:"Agartala"
+    },
+    Class:{
+    type:String,
+    required:true,
+    default:"1"
+  },
+  Board:{
+    type:String,
+    default:"TBSE"
+  },
+  aim:{
+    type:String,
+    required:true
+  },
+  referalCode:{
+    type:String,
+    default:""
+  },
+
+
+})
+
+const UserForm = model("Userform" , FormUser)
+
+App.post("/api/v1/SignupUser" , async(req,res , next)=>{
+  try {
+    let {email , name , phone   , school , address  , Class , Board , referalCode , aim } = req.body
+    console.log(req.body)
+  try {
+      
+     const newUser = await UserForm.create({
+      email , 
+      name , 
+      phone, 
+      school , 
+      address , 
+      Class , 
+      aim , 
+      Board ,
+      referalCode
+      })
+      await newUser.save()
+      new ApiResponse(200 , "Success" , newUser).response(res)
+  } catch (error) {
+    if(error.code){
+      throw new ApiErrorResponse(400 , "User Already exist")
+    }
+    throw new ApiErrorResponse(400 , "Something went wrong")
+
+  }
+  } catch (error) {
+    next(error)
+  }
+})
+
+
+App.get("/api/v1/getInfo" , async(req,res,next)=>{
+  // get all the filtered details based on the details 
+
+})
+
+
+
+App.use(globleErrorHandler)
